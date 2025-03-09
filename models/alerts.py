d12750ff202f72e45c582ff87d9d7004233136d0
Aymen Alsuhaiqi <aymen.alsuhaiqi@gmail.com>
2025-03-10 02:40:58 +0300

    feat:add alert title field

--- models/alerts.py
+++ models/alerts.py
@@ -3,16 +3,18 @@ from odoo import fields,models,api,_
 class Alerts(models.Model):
     _name = 'alerts.alerts'
     _description = 'alerts'
+    _rec_name = 'alert_title'
 
+    alert_title = fields.Char(_('title'),help=_('write alert title here'))
     alert = fields.Text(_('Alert Message'),help=_('write alert message here'),required=True)
-    alert_status = fields.Boolean(_('Alert Status'))
+    alert_status = fields.Boolean(_('Active alert'))
     alert_type = fields.Selection([
-        ('primary', 'primary'),
+        ('info','info'),
         ('success', 'success'),
         ('danger', 'danger'),
-        ('info','info'),
+        ('primary', 'primary'),
         ('warning','warning'),
         ('secondary','secondary'),
-    ],_('Alert Type'),required=True,default='primary')
+    ],_('Alert Type'),required=True,default='info')
     until_date = fields.Datetime(required=True)
-    alert_const = fields.Boolean(_('Is user can close this alert'),default=False)
+    alert_const = fields.Boolean(_('Allow user close alert'),default=True)
